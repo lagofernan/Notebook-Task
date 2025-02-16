d12d8bfc35c07daf943dd3a404c81b66322528f6
Zahriyeh Noah <typeturn@gmail.com>
2025-02-16 13:32:39 -0900

    Create CategoryFilterButton.swift

---
+++ Views/Components/CategoryFilterButton.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct CategoryFilterButton: View {
+    let title: String
+    var color: Color = .blue
+    let isSelected: Bool
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .foregroundColor(isSelected ? .white : color)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(isSelected ? color : color.opacity(0.2))
+                )
+        }
+    }
+}￼Enter
